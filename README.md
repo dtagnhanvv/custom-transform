96714a9c504e994338c547d4331fc8f07e668981
nhanvv <dtag.nhanvv@gmail.com>
2017-02-11 19:33:02 +0700

    add code

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 custom-transform
 ================
 
-A Symfony project created on February 11, 2017, 4:29 pm.
+Installation
+------------
+
+Installation with composer:
+
+``` 
+"repositories": [
+        ...
+        { "type": "vcs", "url": "https://github.com/dtagnhanvv/custom-transform" },
+        ...
+    ],
+
+...
+"require": {
+        ...
+        "nhanvv/custom-transform": "dev-init-code"
+        ...
+    },
+...
+```
+
+
+Next, be sure to enable the bundle in your `app/AppKernel.php` file:
+
+``` php
+public function registerBundles()
+{
+    return array(
+        // ...
+        new \Tagcade\URCustomTransformBundle\URCustomTransformBundle()
+        // ...
+    );
+}
+```
+
+Usage
+-----
--- src/Tagcade/URCustomTransformBundle/EventListener/GetDoctrineListener.php
+++ src/Tagcade/URCustomTransformBundle/EventListener/GetDoctrineListener.php
@@ -2,7 +2,44 @@
 
 namespace Tagcade\URCustomTransformBundle\EventListener;
 
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+
 class GetDoctrineListener
 {
+
+
+    /**
+     * handle event prePersist to detect new ad tag is added, used for updating number of active|paused ad tags of ad network
+     * @param LifecycleEventArgs $args
+     */
+    public function prePersist(LifecycleEventArgs $args)
+    {
+    }
+
+    /**
+     * handle event preUpdate to detect new ad tag is updated, used for updating number of active|paused ad tags of ad network
+     * @param PreUpdateEventArgs $args
+     */
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+    }
+
+    /**
+     * handle event postFlush to update number of active|paused ad tags of ad network for all recorded changedAdNetworks
+     * @param PostFlushEventArgs $args
+     */
+    public function postFlush(PostFlushEventArgs $args)
+    {
+    }
+
+    /**
+     * handle event prePersist to detect new ad tag is added, used for updating number of active|paused ad tags of ad network
+     * @param LifecycleEventArgs $args
+     */
+    public function preSoftDelete(LifecycleEventArgs $args)
+    {
+    }
 
 }
